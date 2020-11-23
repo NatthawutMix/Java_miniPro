47b5c11f05aa9fa665b7017583a16053a9c0074b
NatthawutMix <74909130+NatthawutMix@users.noreply.github.com>
2020-11-23 20:32:07 +0700

    add

---
+++ Project/src/open.java
@@ -0,0 +1,15 @@
+
+public class open {
+	
+	public static void main(String[] args) {
+		
+		windows fram = new windows();
+		
+		fram.setSize(350,640);
+		fram.setLocationRelativeTo(null);
+		fram.setDefaultCloseOperation(fram.EXIT_ON_CLOSE);
+		fram.setVisible(true);
+
+	}
+
+}
